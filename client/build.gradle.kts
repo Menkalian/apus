5cb51ace4077fd93950be5efc9a24e15097941cd
Kilian Krampf <kilian@krampf.de>
2021-04-18 01:42:38 +0200

    Setup Gradle

---
+++ client/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+
+    id("de.menkalian.vela.versioning") apply false
+
+    kotlin("android") apply false
+}
