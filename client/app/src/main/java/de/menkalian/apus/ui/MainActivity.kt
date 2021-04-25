db6f0914d7e4ad75b30b77e2f15401fe43ba739c
Menkalian <kilian@krampf.de>
2021-04-25 14:58:41 +0200

    Add first activity with fragment

---
+++ client/app/src/main/java/de/menkalian/apus/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package de.menkalian.apus.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import de.menkalian.apus.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
